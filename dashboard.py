7ec89bcb5f7079e2961b4a57f6f7ede32436a012
veerandra7 <veerandra.data@gmail.com>
2026-09-21 18:24:34 +0530

    Add dashboard that prints the final calculation report

---
+++ dashboard.py
@@ -0,0 +1,10 @@
+"""Final integration point - displays the results produced by calculator.py."""
+
+from calculator import total, subtraction, multiplication, div
+
+print("*************** DASHBOARD ***************")
+print("Result of addition is       : ", total)
+print("Result of subtraction is    : ", subtraction)
+print("Result of multiplication is : ", multiplication)
+print("Result of division is       : ", div)
+print("*****************************************")
